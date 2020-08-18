38ae2f20c13aac407439a0c25162a082c0c15504
turkerakbulut <turkerakbulut@gmail.com>
2020-08-18 14:13:44 +0300

    .....

---
+++ Tutorial.OpenCV.FaceDetection/Tutorial.OpenCV.FaceDetection/Source.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	//VideoCapture cap(0);
+	//cap.open(0);
+	//Mat img;
+	//CascadeClassifier face_cascade;
+	//face_cascade.load("C:\\opencv341\\sources\\data\\haarcascades\\haarcascade_frontalface_alt2.xml");
+	//for (;;)
+	//{
+	//	cap >> img;
+	//	cap.retrieve(img, CV_CAP_OPENNI_BGR_IMAGE);
+	//	//resize(img, img, Size(1000, 640));
+	//	vector<Rect> faces;
+	//	face_cascade.detectMultiScale(img, faces, 1.1, 2, 0 | CV_HAAR_SCALE_IMAGE, Size(140, 140));
+	//	for (int i = 0; i < faces.size(); i++)
+	//	{
+	//		Point center(faces[i].x + faces[i].width*0.5, faces[i].y + faces[i].height*0.5);
+	//		ellipse(img, center, Size(faces[i].width*0.5, faces[i].height*0.5), 0, 0, 360, Scalar(255, 0, 255), 4, 8, 0);
+	//	}
+	//	imshow("face detect", img);
+	//	const char key = (char)waitKey(5);
+	//	if (key == 27 || key == 'q') // ESC
+	//	{
+	//		cout << "Exit requested" << endl;
+	//		break;
+	//	}
+	}
+}
